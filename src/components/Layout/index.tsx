f56933e1ab30a6b457dc605066d3c319140d537b
zy_nitro <zyascend@qq.com>
2022-06-08 17:00:19 +0800

    feat: 底部导航完善

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,31 +1,56 @@
 import * as React from 'react'
-import { Link, Navigate, Outlet } from 'react-router-dom'
+import { Link, Navigate, Outlet, useLocation } from 'react-router-dom'
 
 import useUserStore from '@/store/useUserStore'
 
 import styles from './index.module.less'
 
 function Layout() {
+  const naviList = [
+    {
+      to: '/app',
+      name: '文档',
+    },
+    {
+      to: '/app/recent',
+      name: '捷径',
+    },
+    {
+      to: '/app/user',
+      name: '个人',
+    },
+  ]
   const token = useUserStore(state => state.token)
+  const loc = useLocation()
+  let left = 0
+  for (let i = 0; i < naviList.length; i++) {
+    if (naviList[i].to === loc.pathname) {
+      left = i * 0.33 * 100
+      break
+    }
+  }
   if (!token) {
     return <Navigate to="/login" />
   }
   return (
     <div className={styles.main}>
       <div className={styles.footer}>
-        <Link to="/app" className={styles.tab}>
-          文档
-        </Link>
-        <Link to="/app/recent" className={styles.tab}>
-          捷径
-        </Link>
-        <Link to="/app/user" className={styles.tab}>
-          个人
-        </Link>
+        <div className={styles.overlay} style={{ left: `${left}%` }}></div>
+        <div className={styles.navigations}>{renderNavigation()}</div>
       </div>
       <Outlet />
     </div>
   )
+  function renderNavigation() {
+    return naviList.map(navi => (
+      <Link
+        to={navi.to}
+        className={`${styles.tab} ${navi.to === loc.pathname ? styles.active : ''}`}
+        key={navi.name}>
+        {navi.name}
+      </Link>
+    ))
+  }
 }
 
 export default Layout
